5e35ce82dc654f33cbc5e60ef34d0ee0a0640926
bhavanih2005 <bhavanih9353@gmail.com>
2025-11-18 20:25:31 +0530

    save to local

--- average.py
+++ average.py
@@ -13,3 +13,26 @@ average = total / len(scores)
 
 print("Sum of scores =", total)
 print("Average of scores =", average)
+import sys
+
+# LOCAL BRANCH FUNCTIONALITY:
+# Sum + Average + Maximum + Minimum
+
+if len(sys.argv) > 1:
+    scores = sys.argv[1:]  # Read from system arguments
+    print("User provided input values:")
+else:
+    print("No command-line input provided. Using default values:")
+    scores = ["10", "20", "30", "40"]  # Default values
+
+# Convert using eval() for calculations
+total = sum(eval(x) for x in scores)
+average = total / len(scores)
+maximum = max(scores, key=eval)
+minimum = min(scores, key=eval)
+
+print("Sum of scores =", total)
+print("Average of scores =", average)
+print("Maximum score =", eval(maximum))
+print("Minimum score =", eval(minimum))
+
